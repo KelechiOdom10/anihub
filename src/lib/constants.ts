93a72931d36f16b9ceda1e123e1de6d7dfa9c7e0
Kelechiodom10 <kelechiodom10@gmail.com>
2024-02-13 12:38:50 +0000

    feat: add a set of tanstack query default options to make app faster

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
+import { type DefaultOptions } from "@tanstack/react-query";
+
 export const APP_TITLE = "Anihub";
 export const DATABASE_PREFIX = "anihub";
 export const EMAIL_SENDER = '"Anihub" <noreply@anihub.com>';
@@ -10,3 +12,12 @@ export const redirects = {
   toVerify: "/verify-email",
   afterVerify: "/",
 } as const;
+
+export const QUERY_OPTIONS_DEFAULT: DefaultOptions["queries"] = {
+  retry: false,
+  staleTime: 15 * 60 * 1000, // 15 minutes
+  enabled: true,
+  refetchInterval: false,
+  refetchOnMount: false,
+  refetchOnWindowFocus: false,
+};
