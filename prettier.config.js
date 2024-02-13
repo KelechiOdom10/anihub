969143e23d6adf9e57818d52f439b7c23049ae24
Kelechiodom10 <kelechiodom10@gmail.com>
2024-02-13 12:41:17 +0000

    chore: properly setup prettier in project

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 80,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
 };
 
 export default config;
