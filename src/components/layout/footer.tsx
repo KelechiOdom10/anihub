1e385efefbf6a63bee246c956c3cca72389cb889
Kelechiodom10 <kelechiodom10@gmail.com>
2024-03-30 01:26:50 +0000

    feat: create footer

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { Logo } from "../logo";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
+
+const socials = [
+  {
+    name: "GitHub",
+    url: "https://github.com/KelechiOdom10/anihub",
+    icon: GitHubLogoIcon,
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/kelechi-odom/",
+    icon: LinkedInLogoIcon,
+  },
+] as const;
+
+export const Footer = () => {
+  return (
+    <footer className="border-t border-t-zinc-700/80 px-4 py-6">
+      <div className="container flex items-center justify-between p-0">
+        <Link href="/">
+          <Logo className="h-9 w-auto fill-white" />
+        </Link>
+        <div className="flex gap-2">
+          {socials.map(({ name, url, icon: Icon }) => (
+            <Button key={name} variant="ghost" size="icon" asChild>
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                <Icon className="h-6 w-6" />
+              </a>
+            </Button>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
