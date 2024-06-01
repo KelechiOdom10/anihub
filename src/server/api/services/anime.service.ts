99857148f7cf248d5ff9a468137d669c5a415e6b
Kelechiodom10 <kelechiodom10@gmail.com>
2024-06-01 12:01:36 +0100

    refactor: rename anime-service.ts to anime.service.ts

--- src/server/api/services/anime.service.ts
+++ src/server/api/services/anime.service.ts
@@ -1,5 +1,5 @@
 import createClient from "openapi-fetch";
-import type { paths } from "../../jikan-schema.ts";
+import type { paths } from "../../jikan-schema";
 
 export const animeService = createClient<paths>({
   baseUrl: "https://api.jikan.moe/v4",
