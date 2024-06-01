6de1d2adc69113f150936769399de3702ebb65fc
Kelechiodom10 <kelechiodom10@gmail.com>
2024-06-01 12:45:30 +0100

    chore: update npm dependencies and TypeScript version

--- src/lib/auth/validate-request.ts
+++ src/lib/auth/validate-request.ts
@@ -13,7 +13,7 @@ export const uncachedValidateRequest = async (): Promise<
   const result = await lucia.validateSession(sessionId);
   // next.js throws when you attempt to set cookie when rendering page
   try {
-    if (result.session && result.session.fresh) {
+    if (result.session?.fresh) {
       const sessionCookie = lucia.createSessionCookie(result.session.id);
       cookies().set(
         sessionCookie.name,
@@ -21,6 +21,7 @@ export const uncachedValidateRequest = async (): Promise<
         sessionCookie.attributes
       );
     }
+
     if (!result.session) {
       const sessionCookie = lucia.createBlankSessionCookie();
       cookies().set(
