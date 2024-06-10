f09813f4c83daa40dffb5586dba3eee8c4ee3f26
Kelechiodom10 <kelechiodom10@gmail.com>
2024-06-10 16:48:19 +0100

    refactor: remove all unused trpc code

--- src/server/api/root.ts
+++
@@ -1,8 +0,0 @@
-import { userRouter } from "./routers/user";
-import { createTRPCRouter } from "./trpc";
-
-export const appRouter = createTRPCRouter({
-  user: userRouter,
-});
-
-export type AppRouter = typeof appRouter;
