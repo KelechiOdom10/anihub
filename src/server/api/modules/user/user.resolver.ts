fc5249c3a35564186df80de3c6f8a16af670f603
Kelechiodom10 <kelechiodom10@gmail.com>
2024-06-10 16:51:13 +0100

    feat: add user resolver and schema updates

---
+++ src/server/api/modules/user/user.resolver.ts
@@ -0,0 +1,36 @@
+import { eq } from "drizzle-orm";
+
+import { builder } from "../../builder";
+
+import { users, type User as UserType } from "~/server/db/schema";
+
+export const UserObjectRef = builder.objectRef<UserType>("User");
+
+export const User = builder.objectType(UserObjectRef, {
+  description: "User object",
+  fields: (t) => ({
+    id: t.exposeID("id"),
+    email: t.exposeString("email"),
+    emailVerified: t.exposeBoolean("emailVerified"),
+    avatar: t.exposeString("avatar", { nullable: true }),
+    createdAt: t.exposeString("createdAt"),
+    updatedAt: t.exposeString("updatedAt"),
+  }),
+});
+
+builder.queryField("me", (t) =>
+  t.field({
+    type: User,
+    description: "Get current logged in user",
+    nullable: true,
+    resolve: async (_parent, _args, ctx) => {
+      if (!ctx.session) {
+        throw new Error("Unauthorized");
+      }
+
+      return await ctx.db.query.users.findFirst({
+        where: eq(users.id, ctx.session.userId),
+      });
+    },
+  })
+);
--- src/server/api/schema.ts
+++ src/server/api/schema.ts
@@ -2,5 +2,6 @@ import { builder } from "./builder";
 import "./modules/anime";
 import "./modules/character";
 import "./modules/genre";
+import "./modules/user";
 
 export const schema = builder.toSchema();
