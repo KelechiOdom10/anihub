b46707a4dc3dd0662f5f8d7a227ad5208b20abaa
Kelechiodom10 <kelechiodom10@gmail.com>
2024-06-11 12:25:29 +0100

    feat: add genres/categories section to home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import { type Metadata } from "next";
+import { Suspense } from "react";
 
 import { AnimeCarousel } from "./_components/anime-carousel";
+import { GenresSection } from "./_components/genres-section";
 import { Hero } from "./_components/hero";
 
 import { getClient } from "~/graphql/client";
-import { TopAnimeQuery } from "~/graphql/queries";
+import { GenresQuery, TopAnimeQuery } from "~/graphql/queries";
 
 export const metadata: Metadata = {
   title: "Next.js Lucia Auth Starter Template",
@@ -13,14 +15,17 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
+  const { data: genreData } = await getClient().query(GenresQuery, {});
   const { data: recommended } = await getClient().query(TopAnimeQuery, {});
-  const { data: popular } = await getClient().query(TopAnimeQuery, {
+  const { data: popularData } = await getClient().query(TopAnimeQuery, {
     query: {
       filter: "bypopularity",
       limit: 10,
     },
   });
   const heroAnime = recommended?.getTopAnimes?.[0];
+  const shuffledGenres =
+    genreData?.getGenres?.sort(() => Math.random() - 0.5).slice(0, 3) ?? [];
 
   return (
     <>
@@ -33,12 +38,19 @@ export default async function Home() {
           />
         </div>
       )}
-      {popular?.getTopAnimes && (
+      {popularData?.getTopAnimes && (
         <div className="container mx-auto py-8">
           <AnimeCarousel
             heading="Popular Anime"
-            animeList={popular.getTopAnimes}
+            animeList={popularData.getTopAnimes}
           />
+        </div>
+      )}
+      {genreData?.getGenres && (
+        <div className="container mx-auto py-8">
+          <Suspense>
+            <GenresSection genres={shuffledGenres} />
+          </Suspense>
         </div>
       )}
     </>
