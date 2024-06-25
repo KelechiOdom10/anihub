dde6363dad032a8ed4fe3cfe81bbe33d68990155
Kelechiodom10 <kelechiodom10@gmail.com>
2024-06-25 01:11:44 +0100

    style: update Info Card styling

--- src/components/ui/info-card.tsx
+++ src/components/ui/info-card.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { type ReactNode } from "react";
 
 import { Meteors } from "./meteors";
 
 interface InfoCardProps {
-  heading: string;
+  heading: ReactNode;
   images: Array<{
     url: string;
     alt: string;
@@ -12,7 +13,7 @@ interface InfoCardProps {
 
 export const InfoCard = ({ heading, images }: InfoCardProps) => {
   return (
-    <div className="relative flex h-full w-full flex-col items-center gap-8 overflow-hidden rounded-lg bg-zinc-800 px-6 pt-12 transition-transform duration-300 ease-in-out hover:scale-105">
+    <div className="relative flex h-full w-full flex-col items-center gap-8 overflow-hidden rounded-lg bg-zinc-900 px-6 pt-12 transition-transform duration-300 ease-in-out hover:scale-105">
       <Meteors number={10} />
       <h4 className="line-clamp-2 max-w-48 text-center text-xl font-medium">
         {heading}
@@ -25,7 +26,7 @@ export const InfoCard = ({ heading, images }: InfoCardProps) => {
             key={index}
             src={image.url}
             alt={image.alt}
-            className={`aspect-[3/4] w-[110px] transform overflow-hidden rounded-lg border-[5px] border-zinc-800 shadow-lg transition-transform duration-300 ease-in-out hover:scale-105`}
+            className={`aspect-[3/4] w-[110px] transform overflow-hidden rounded-lg border-[5px] border-zinc-900 shadow-lg transition-transform duration-300 ease-in-out hover:scale-105`}
             // transform to be at a slight angle
             style={{
               transform: `rotate(${index % 2 === 0 ? 5 : -5}deg)`,
