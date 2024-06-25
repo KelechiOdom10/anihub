00f3c6db85d47b77250399fc15a7d358f39fbc0e
Kelechiodom10 <kelechiodom10@gmail.com>
2024-06-25 01:13:27 +0100

    feat: add Characters Marquee component

---
+++ src/app/(main)/_components/characters-marquee.tsx
@@ -0,0 +1,37 @@
+import { type FragmentOf } from "gql.tada";
+import { type FunctionComponent } from "react";
+
+import { CharacterCard } from "~/components/ui/character-card";
+import Marquee from "~/components/ui/marquee";
+import { type CharacterPreview } from "~/graphql/fragments";
+
+interface CharactersMarqueeProps {
+  characters: NonNullable<Array<FragmentOf<typeof CharacterPreview>>>;
+}
+
+export const CharactersMarquee: FunctionComponent<CharactersMarqueeProps> = ({
+  characters,
+}) => {
+  const firstRow = characters.slice(0, 5);
+  const secondRow = characters.slice(5, characters.length - 1);
+
+  return (
+    <section className="flex flex-col space-y-8">
+      <h3 className="text-xl font-bold text-white">Trending Characters</h3>
+      <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden rounded-lg pb-20 md:shadow-xl">
+        <Marquee pauseOnHover className="[--duration:100s]">
+          {firstRow.map((character) => (
+            <CharacterCard key={character.id} character={character} />
+          ))}
+        </Marquee>
+        <Marquee reverse pauseOnHover className="[--duration:100s]">
+          {secondRow.map((character) => (
+            <CharacterCard key={character.id} character={character} />
+          ))}
+        </Marquee>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-background dark:from-background"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-background dark:from-background"></div>
+      </div>
+    </section>
+  );
+};
