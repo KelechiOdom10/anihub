d61a91d8c7e06cc531f2235b15438d9eef0708ef
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-11 17:31:10 -0400

    refactor: export shared Pagination type

--- src/server/api/modules/shared.ts
+++ src/server/api/modules/shared.ts
@@ -1,13 +1,18 @@
 import { builder } from "../builder";
 
+import { type components } from "~/server/jikan-schema";
+
+type Pagination = {
+  has_next_page?: boolean;
+  last_visible_page?: number;
+};
+
 type PaginationItemsType = {
   count?: number;
   total?: number;
   per_page?: number;
 };
-type PaginationResultType = {
-  last_visible_page?: number;
-  has_next_page?: boolean;
+type PaginationResultType = Pagination & {
   items: PaginationItemsType;
 };
 export type TitleType = components["schemas"]["title"];
@@ -15,6 +20,7 @@ type CommonImageType = NonNullable<
   components["schemas"]["common_images"]["jpg"]
 >;
 
+export const Pagination = builder.objectRef<Pagination>("Pagination");
 export const PaginationItems =
   builder.objectRef<PaginationItemsType>("PaginationItems");
 export const PaginationResult =
@@ -27,6 +33,27 @@ export const Title = builder.objectRef<TitleType>("AnimeTitle").implement({
     title: t.exposeString("title", { nullable: true }),
   }),
 });
+
+export const CommonImage = builder
+  .objectRef<CommonImageType>("CommonImage")
+  .implement({
+    description: "Common Image object",
+    fields: (t) => ({
+      imageUrl: t.exposeString("image_url", { nullable: true }),
+    }),
+  });
+
+export const SearchSortEnum = builder.enumType("SearchSortEnum", {
+  values: ["asc", "desc"] as const,
+});
+
+builder.objectType(Pagination, {
+  fields: (t) => ({
+    hasNextPage: t.exposeBoolean("has_next_page", { nullable: true }),
+    lastVisiblePage: t.exposeInt("last_visible_page", { nullable: true }),
+  }),
+});
+
 builder.objectType(PaginationItems, {
   fields: (t) => ({
     count: t.exposeInt("count", { nullable: true }),
