906931887accef1c048d4533655d5efdbbd4d846
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-10 21:59:21 -0400

    feat: add carousel component for anime previews on home page

--- src/app/(main)/_components/anime-carousel.tsx
+++ src/app/(main)/_components/anime-carousel.tsx
@@ -1,27 +1,45 @@
 "use client";
 
-import { type FragmentOf } from "gql.tada";
 import Link from "next/link";
 
 import { AnimeCard } from "~/components/ui/anime-card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "~/components/ui/carousel";
 import { type AnimePreview } from "~/graphql/fragments";
 
 interface AnimeCarouselProps {
   heading: string;
-  animeList: Array<FragmentOf<typeof AnimePreview>>;
+  animeList: Array<AnimePreview>;
 }
 
 export const AnimeCarousel = ({ heading, animeList }: AnimeCarouselProps) => {
   return (
     <section className="flex flex-col space-y-8">
       <h3 className="text-xl font-bold text-white">{heading}</h3>
-      <div className="relative flex gap-6 overflow-x-scroll">
-        {animeList.map((anime) => (
-          <Link key={anime.id} href={`/anime/${anime.id}`}>
-            <AnimeCard anime={anime} />
-          </Link>
-        ))}
-      </div>
+      <Carousel
+        opts={{
+          align: "start",
+          dragFree: true,
+        }}
+        className="w-full"
+      >
+        <CarouselContent>
+          {animeList.map((anime) => (
+            <CarouselItem key={anime.id} className="basis-auto p-2">
+              <Link href={`/anime/${anime.id}`}>
+                <AnimeCard anime={anime} className="w-56" />
+              </Link>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="hidden md:block" />
+        <CarouselNext className="hidden md:block" />
+      </Carousel>
     </section>
   );
 };
