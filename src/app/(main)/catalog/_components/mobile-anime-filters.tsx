50baa2d6ed3dcdeaf56352b652593875a7aa22dd
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-10 22:10:27 -0400

    feat: add component for mobile anime filters seen on smaller screens

---
+++ src/app/(main)/catalog/_components/mobile-anime-filters.tsx
@@ -0,0 +1,28 @@
+import { type FunctionComponent } from "react";
+
+import { AnimeFilters, type AnimeFiltersProps } from "./anime-filters";
+
+import { FunnelIcon } from "~/components/icons";
+import { Button } from "~/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "~/components/ui/sheet";
+
+export const MobileAnimeFilter: FunctionComponent<AnimeFiltersProps> = ({
+  initialFilters,
+  genres,
+}) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="default" size="icon" className="flex md:hidden">
+          <FunnelIcon className="h-5 w-5" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent
+        className="flex w-10/12 flex-col overflow-y-auto p-4 sm:max-w-screen-md md:hidden"
+        overlayClassName="md:hidden"
+      >
+        <AnimeFilters initialFilters={initialFilters} genres={genres} />
+      </SheetContent>
+    </Sheet>
+  );
+};
