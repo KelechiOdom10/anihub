35b388d9bc57a261447d9cc072af63072031921d
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-12 16:53:07 -0400

    refactor: update schema to include Producer module

--- src/server/api/schema.ts
+++ src/server/api/schema.ts
@@ -3,10 +3,12 @@ import { printSchema, lexicographicSortSchema } from "graphql";
 import { writeFileSync } from "fs";
 
 import { builder } from "./builder";
+import "./modules/shared";
 import "./modules/anime";
 import "./modules/character";
 import "./modules/genre";
 import "./modules/user";
+import "./modules/producer";
 
 export const schema = builder.toSchema();
 
