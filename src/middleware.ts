2ab7b61890e15e053eb0343b9288bfdab6a0b02e
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-15 01:47:43 -0400

    refactor: update middleware config to include additional routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,5 +24,7 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 export const config = {
   matcher: [
     "/((?!api|static|.*\\..*|_next|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/",
+    "/catalog",
   ],
 };
