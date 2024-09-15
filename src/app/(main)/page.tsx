ae713460b3baab4ec84ed359263b7568850b6d6f
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-15 00:27:34 -0400

    refactor: optimize Home component data fetching

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,11 @@
 import { type Metadata } from "next";
-import { Suspense } from "react";
 
 import { AnimeCarousel } from "./_components/anime-carousel";
 import { CharactersMarquee } from "./_components/characters-marquee";
-import { GenresSection } from "./_components/genres-section";
-import { GenresSectionSkeleton } from "./_components/genres-section-skeleton";
 import { Hero } from "./_components/hero";
 
 import { getClient } from "~/graphql/client";
-import {
-  GenresQuery,
-  TopAnimeQuery,
-  TopCharactersQuery,
-} from "~/graphql/queries";
+import { TopAnimeQuery, TopCharactersQuery } from "~/graphql/queries";
 
 export const metadata: Metadata = {
   title: "Anihub • Social anime discovery",
@@ -20,49 +13,30 @@ export const metadata: Metadata = {
     "Anihub is a social platform for discovering and sharing your taste in anime. Use it as a diary to record your opinion about animes as you watch them, or just to keep track of animes you’ve seen in the past. Rate, review and tag animes as you add them. Find and follow your friends to see what they’re enjoying. Keep a watchlist of animes you’d like to see, and create lists/collections on any topic.",
 };
 
-export const dynamic = "force-dynamic";
-
 export default async function Home() {
-  const [
-    { data: genreData },
-    { data: recommended },
-    { data: popularData },
-    { data: trendingData },
-    { data: characterData },
-  ] = await Promise.all([
-    getClient().query(GenresQuery, {}),
-    getClient().query(TopAnimeQuery, {}),
-    getClient().query(TopAnimeQuery, {
-      query: {
-        filter: "bypopularity",
-        limit: 10,
-      },
-    }),
-    getClient().query(TopAnimeQuery, {
-      query: {
-        filter: "favorite",
-        limit: 10,
-      },
-    }),
-    getClient().query(TopCharactersQuery, {
-      query: {
-        filter: "trending",
-        limit: 10,
-      },
-    }),
-  ]);
-
-  console.log({
-    genreData,
-    recommended,
-    popularData,
-    trendingData,
-    characterData,
+  // const { data: genreData } = await getClient().query(GenresQuery, {});
+  const { data: recommended } = await getClient().query(TopAnimeQuery, {});
+  const { data: popularData } = await getClient().query(TopAnimeQuery, {
+    query: {
+      filter: "bypopularity",
+      limit: 10,
+    },
   });
-
+  // const { data: trendingData } = await getClient().query(TopAnimeQuery, {
+  //   query: {
+  //     filter: "favorite",
+  //     limit: 10,
+  //   },
+  // });
+  const { data: characterData } = await getClient().query(TopCharactersQuery, {
+    query: {
+      filter: "trending",
+      limit: 10,
+    },
+  });
   const heroAnime = recommended?.getTopAnimes?.[4];
-  const shuffledGenres =
-    genreData?.getGenres?.sort(() => Math.random() - 0.5).slice(0, 3) ?? [];
+  // const shuffledGenres =
+  //   genreData?.getGenres?.sort(() => Math.random() - 0.5).slice(0, 3) ?? [];
 
   return (
     <>
@@ -83,21 +57,21 @@ export default async function Home() {
           />
         </div>
       )}
-      {genreData?.getGenres && (
+      {/* {genreData?.getGenres && (
         <div className="container mx-auto py-8">
           <Suspense fallback={<GenresSectionSkeleton />}>
             <GenresSection genres={shuffledGenres} />
           </Suspense>
         </div>
-      )}
-      {trendingData?.getTopAnimes && (
+      )} */}
+      {/* {trendingData?.getTopAnimes && (
         <div className="container mx-auto py-8">
           <AnimeCarousel
             heading="Trending Now"
             animeList={trendingData.getTopAnimes}
           />
         </div>
-      )}
+      )} */}
       {characterData?.getTopCharacters && (
         <div className="container mx-auto py-8">
           <CharactersMarquee characters={characterData.getTopCharacters} />
