d2a3772480e3e21a1d60cce673e020b78485917a
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-15 09:17:14 -0400

    refactor: add getUrl function to graphql route

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,6 +1,7 @@
 // Next.js Custom Route Handler: https://nextjs.org/docs/app/building-your-application/routing/router-handlers
 import { createYoga } from "graphql-yoga";
 
+import { getUrl } from "~/graphql/shared";
 import { createContext } from "~/server/api/context";
 import { schema } from "~/server/api/schema";
 
@@ -8,13 +9,15 @@ const { handleRequest } = createYoga({
   schema: schema,
 
   // While using Next.js file convention for routing, we need to configure Yoga to use the correct endpoint
-  graphqlEndpoint: "/api/graphql",
+  graphqlEndpoint: getUrl(),
 
   // Yoga needs to know how to create a valid Next response
-  fetchAPI: { Response },
+  fetchAPI: { Response, Request },
 
   // Context
   context: createContext,
+
+  logging: true,
 });
 
 export {
