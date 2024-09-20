975dc46f316b319d08405eb92b6936ec8de61080
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-20 00:31:07 -0400

    refactor: add custom Play Icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -119,7 +119,6 @@ export {
   Pencil2Icon,
   UpdateIcon,
   CheckCircledIcon,
-  PlayIcon,
   TrashIcon,
   ArchiveIcon,
   ResetIcon,
@@ -190,3 +189,28 @@ export function DescendingOrderIcon(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export const PlayIcon = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
+  (props, ref) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      ref={ref}
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
+      />
+    </svg>
+  )
+);
