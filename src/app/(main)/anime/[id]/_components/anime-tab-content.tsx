c87e2c92fca005ef260775867c44a084546c8b32
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-20 00:42:51 -0400

    feat: add AnimeTabContent component for displaying anime tab content

---
+++ src/app/(main)/anime/[id]/_components/anime-tab-content.tsx
@@ -0,0 +1,32 @@
+import { type FunctionComponent } from "react";
+
+import { AnimeOverview } from "./anime-overview";
+
+import { type AnimeQueryData } from "~/graphql/queries";
+
+interface AnimeTabContentProps {
+  animeId: string;
+  currentTab: string;
+  anime: AnimeQueryData;
+}
+
+export const AnimeTabContent: FunctionComponent<AnimeTabContentProps> = ({
+  animeId,
+  currentTab,
+  anime,
+}) => {
+  switch (currentTab) {
+    case "overview":
+      return <AnimeOverview anime={anime} />;
+    case "relations":
+      return <div>Relations {animeId}</div>;
+    case "characters":
+      return <div>Characters {animeId}</div>;
+    case "staff":
+      return <div>Staff {animeId}</div>;
+    case "reviews":
+      return <div>Reviews {animeId}</div>;
+    default:
+      return <AnimeOverview anime={anime} />;
+  }
+};
