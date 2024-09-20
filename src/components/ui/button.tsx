f41b620047106320949278b7bc59733228e1b667
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-20 00:34:17 -0400

    refactor: update Button component to support left and right icons

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import { Slot } from "@radix-ui/react-slot";
+import { Slot, Slottable } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
@@ -42,17 +42,36 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  leftIcon?: React.ReactNode;
+  rightIcon?: React.ReactNode;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, fullWidth, asChild = false, ...props }, ref) => {
+  (
+    {
+      className,
+      variant,
+      size,
+      fullWidth,
+      children,
+      leftIcon,
+      rightIcon,
+      asChild = false,
+      ...props
+    },
+    ref
+  ) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, fullWidth, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {leftIcon && <span className="mr-2">{leftIcon}</span>}
+        <Slottable>{children}</Slottable>
+        {rightIcon && <span className="ml-2">{rightIcon}</span>}
+      </Comp>
     );
   }
 );
