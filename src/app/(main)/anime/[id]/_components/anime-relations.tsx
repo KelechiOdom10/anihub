c74e12971575ad3c80861b5687e48533c8be7d12
Kelechi Odom <kelechiodom10@gmail.com>
2024-09-20 22:52:18 -0400

    feat: add AnimeRelations component for displaying related anime

---
+++ src/app/(main)/anime/[id]/_components/anime-relations.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { useQuery } from "urql";
+
+import { AnimeCard, AnimeCardSkeleton } from "~/components/ui/anime-card";
+import { AnimeRelationsQuery } from "~/graphql/queries";
+
+export const AnimeRelations = ({ animeId }: { animeId: number }) => {
+  const [{ data, fetching }] = useQuery({
+    query: AnimeRelationsQuery,
+    variables: { id: animeId },
+  });
+
+  const animeRelations = data?.getAnimeRelations ?? [];
+  const noRelations = !fetching && animeRelations.length === 0;
+
+  return (
+    <>
+      {noRelations && <p>No related anime found.</p>}
+      {animeRelations.length > 0 && (
+        <div className="grid grid-cols-2 gap-5 md:flex md:flex-wrap">
+          {animeRelations.map((anime) => (
+            <Link key={anime.id} href={`/anime/${anime.id}`}>
+              <AnimeCard anime={anime} className="md:w-52" />
+            </Link>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export const AnimeRelationsSkeleton = () => (
+  <div className="grid grid-cols-2 gap-5 md:flex md:flex-wrap">
+    {Array(3)
+      .fill(0)
+      .map((i) => (
+        <AnimeCardSkeleton key={i} className="md:w-52" />
+      ))}
+  </div>
+);
