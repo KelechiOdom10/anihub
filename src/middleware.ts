5a4e6aacc3be870424138560bc50030fcc1b796f
Kelechi Odom <kelechiodom10@gmail.com>
2024-10-25 01:54:56 +0100

    refactor: move verifyRequestOrigin function out of lucia package

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
-// middleware.ts
-import { verifyRequestOrigin } from "lucia";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+
+import { verifyRequestOrigin } from "./lib/auth/request";
 
 export async function middleware(request: NextRequest): Promise<NextResponse> {
   if (request.method === "GET") {
