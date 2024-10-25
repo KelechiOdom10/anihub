df05b9eeb229bd22efa00e7d72def5068e459b04
Kelechi Odom <kelechiodom10@gmail.com>
2024-10-25 18:40:08 +0100

    feat: add component for displaying loading state during auth check

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,12 @@
-import type { ReactNode } from "react";
+import { Suspense, type ReactNode } from "react";
+
+import { AuthLoading } from "./_components/auth-loading";
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="grid min-h-screen place-items-center p-4">{children}</div>
+    <div className="grid min-h-screen place-items-center p-4">
+      <Suspense fallback={<AuthLoading />}>{children}</Suspense>
+    </div>
   );
 };
 
