d48329f0c31d6064fbb404330d68d2e4637cb7a4
Kelechi Odom <kelechiodom10@gmail.com>
2025-01-10 16:21:07 +0100

    feat: create query for getting current logged in users collections

---
+++ src/graphql/queries/get-user-collections.ts
@@ -0,0 +1,21 @@
+import { graphql, type ResultOf, type VariablesOf } from "gql.tada";
+
+export const GetMyCollectionsQuery = graphql(`
+  query GetMyCollections($query: CollectionQueryParams, $animeId: Int) {
+    getMyCollections(query: $query) {
+      id
+      name
+      description
+      isPublic
+      userId
+      createdAt
+      updatedAt
+      hasAnime(animeId: $animeId)
+    }
+  }
+`);
+
+export type GetMyCollectionsResult = ResultOf<typeof GetMyCollectionsQuery>;
+export type GetMyCollectionsQueryParams = VariablesOf<
+  typeof GetMyCollectionsQuery
+>;
