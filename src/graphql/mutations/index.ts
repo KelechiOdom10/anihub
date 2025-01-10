ef5e1835bc4090c11729a0a1e2c1518fa1981a18
Kelechi Odom <kelechiodom10@gmail.com>
2025-01-10 16:39:41 +0100

    chore: export mutations

---
+++ src/graphql/mutations/index.ts
@@ -0,0 +1,6 @@
+export * from "./add-anime-to-collection";
+export * from "./add-anime-to-new-collection";
+export * from "./bulk-update-collections";
+export * from "./create-collection";
+export * from "./remove-anime-from-collection";
+export * from "./update-collection";
