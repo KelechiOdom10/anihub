a9e895489dc6c0c8db76fd13a52560d89aaf578d
Kelechi Odom <kelechiodom10@gmail.com>
2025-01-11 17:38:52 +0100

    feat: include selected state in anime card

--- src/components/ui/anime-card.tsx
+++ src/components/ui/anime-card.tsx
@@ -1,3 +1,4 @@
+import { CheckIcon } from "@radix-ui/react-icons";
 import Image from "next/image";
 
 import { type AnimePreview } from "~/graphql/fragments";
@@ -9,9 +10,15 @@ interface AnimeCardProps {
   anime: AnimePreview;
   className?: string;
   loading: "eager" | "lazy";
+  selected?: boolean;
 }
 
-export const AnimeCard = ({ anime, className, loading }: AnimeCardProps) => {
+export const AnimeCard = ({
+  anime,
+  className,
+  loading,
+  selected,
+}: AnimeCardProps) => {
   const { titles, genres, year } = anime;
   const title = getEnglishTitle((titles ?? []) as TitleType[]);
   const genre = genres?.[0]?.name ?? "N/A";
@@ -21,6 +28,8 @@ export const AnimeCard = ({ anime, className, loading }: AnimeCardProps) => {
     <div
       className={cn(
         "relative aspect-[2/3] w-full overflow-hidden rounded-xl bg-gradient-to-b from-transparent via-background/10 to-background/75 shadow-lg",
+        selected &&
+          "bg-gradient-to-t from-transparent via-background/10 to-background/75 ring-[1.5px] ring-primary ring-offset-2 ring-offset-background",
         className
       )}
     >
@@ -34,11 +43,16 @@ export const AnimeCard = ({ anime, className, loading }: AnimeCardProps) => {
         alt={title}
       />
       <div className="absolute inset-0 isolate flex items-end p-3">
-        <div className="grid grid-cols-1 gap-1">
+        <div className="grid grid-cols-1 gap-1 text-left">
           <p className="font-semibold text-white">{title}</p>
           <p className="text-sm text-slate-300">{extraInfo}</p>
         </div>
       </div>
+      {selected && (
+        <div className="absolute right-2 top-2 rounded-full bg-primary p-1">
+          <CheckIcon className="h-4 w-4 text-zinc-600" />
+        </div>
+      )}
     </div>
   );
 };
